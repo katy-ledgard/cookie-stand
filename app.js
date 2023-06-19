5adaa9dba8e46f49cd5de4408259f7b3b7413ddf
Katy <katy.ledgard@gmail.com>
2023-06-19 14:09:03 +0100

    add an object in js

--- app.js
+++ app.js
@@ -1,4 +1,32 @@
 "use strict";
 
-console.log("Hey Cookie.")
+console.log("Hey Cookie.");
 
+const hours = [
+  "6am",
+  "7am",
+  "8am",
+  "9am",
+  "10am",
+  "11am",
+  "12pm",
+  "1pm",
+  "2pm",
+  "3pm",
+  "4pm",
+  "5pm",
+  "6pm",
+  "7pm",
+];
+
+const Seattle = {
+  storeName: "Seattle",
+  minCustPerHour: 23,
+  maxCustPerHour: 65,
+  avgCookiesPerCust: 6.3,
+  customersEachHour: [33, 31, 33, 31, 33, 31, 33, 31, 33, 31, 33, 31, 33, 31],
+  cookiesEachHour: [
+    198, 200, 198, 200, 198, 200, 198, 200, 198, 200, 198, 200, 198, 200,
+  ],
+  totalDailyCookies: 2786,
+};
